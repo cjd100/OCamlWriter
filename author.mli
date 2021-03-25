e78930ca9d7f63337da85d052f4250a9901fa0e8
Brandon Diaz <bld68@cornell.edu>
2021-03-24 22:32:59 -0400

    Implemented a basic text editor GUI in gui.ml - Created main window - Added menu options on top bar (Some of which are unimplemented) - Added the main text box widget

--- author.mli
+++ author.mli
@@ -1,13 +1,11 @@
 (********************************************
 
-CS 3110 Spring 2021 Final Project
+  CS 3110 Spring 2021 Final Project
 
-@authors Nicholas Romanofsky (njr67)
-         Christian Donovan (cjd237)
-         Brandon Diaz (bld68) 
+  @authors Nicholas Romanofsky (njr67) Christian Donovan (cjd237)
+  Brandon Diaz (bld68)
 
-********************************************)
-
+  ********************************************)
 
 (** [hours_worked] is the number of hours the group worked in total. *)
 val hours_worked : int
