fcb83f4f86d07799c9179c311b112b406587dd67
Nick Romanofsky <njr67@cornell.edu>
2021-03-24 20:43:24 -0400

    Implemented author signature

---
+++ author.mli
@@ -0,0 +1,13 @@
+(********************************************
+
+CS 3110 Spring 2021 Final Project
+
+@authors Nicholas Romanofsky (njr67)
+         Christian Donovan (cjd237)
+         Brandon Diaz (bld68) 
+
+********************************************)
+
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int
