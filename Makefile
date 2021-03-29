4611b0520d4dea4c8d558b3c02562d2c38baa445
Brandon Diaz <bld68@cornell.edu>
2021-03-29 18:15:51 -0400

    -Implemented GUI functionality for the Open File menu option (ctrl+O) -Created a new makefile target (gui_temp) for GUI testing -TODO: add file operations to the Open File menu option

--- Makefile
+++ Makefile
@@ -4,11 +4,23 @@ MLS=$(MODULES:=.ml)
 MLIS=$(MODULES:=.mli)
 TEST=test.byte
 MAIN=gui.byte
-OCAMLBUILD=ocamlbuild -use-ocamlfind -r
-
+#OCAMLBUILD=ocamlbuild -use-ocamlfind -r 
+OCAMLBUILD = ocamlfind ocamlc -g 
 
 default: build 
 	OCAMLRUNPARAM=b utop
+
+# TEMPORARY: Displays the GUI, but there's probably a better way to do it
+#
+# If you are on windows, you need xming or another x-server program open for 
+# this to work
+gui_temp:
+	ocamlfind ocamlc -g -package lablgtk2 -linkpkg gui.mli -o gui
+	ocamlfind ocamlc -g -package lablgtk2 -linkpkg gui.ml -o gui 
+	./gui
+	rm gui.cmi
+	rm gui.cmo
+	rm gui
 
 build:
 	$(OCAMLBUILD) $(OBJECTS)
@@ -23,11 +35,11 @@ docs: docs-public docs-private
 	
 docs-public: build
 	mkdir -p _doc.public
-	ocamlfind ocamldoc -I _build -package Core,lablgtk2 \
+	ocamlfind ocamldoc -I _build -package core,lablgtk2 \
 		-html -stars -d _doc.public $(MLIS)
 
 docs-private: build
 	mkdir -p _doc.private
-	ocamlfind ocamldoc -I _build -package Core,lablgtk2 \
+	ocamlfind ocamldoc -I _build -package core,lablgtk2 \
 		-html -stars -d _doc.private \
 		-inv-merge-ml-mli -m A $(MLIS) $(MLS)
--- gui.ml
+++ gui.ml
@@ -14,6 +14,29 @@ let win_dim = (400, 400)
    the text field [field] *)
 let insert_text text field = field#buffer#set_text text
 
+(** [load_file parent] opens up a new window, with the parent window
+    [parent]. The new window is a file selection GUI that loads the
+    selected file into the document. *)
+let load_file parent =
+  let get_filename = function Some f -> f | None -> "" in
+  (* window that appears when the request file command is issued*)
+  let open_file_window =
+    GWindow.file_chooser_dialog ~action:`OPEN ~width:300 ~height:300
+      ~title:"Open file" ~parent ()
+  in
+  open_file_window#add_button_stock `CANCEL `CANCEL;
+  open_file_window#add_select_button_stock `OPEN `OPEN;
+  begin
+    match open_file_window#run () with
+    | `OPEN ->
+        let filename = get_filename open_file_window#filename in
+        (* TODO: Replace the print statement below with something that
+           loads the file [filename] into the document*)
+        print_endline ("The file you selected was: " ^ filename)
+    | `DELETE_EVENT | `CANCEL -> ()
+  end;
+  open_file_window#destroy ()
+
 let main () =
   let editor_window =
     GWindow.window ~width:(fst win_dim) ~height:(snd win_dim)
@@ -45,13 +68,12 @@ let main () =
 
   (* File menu *)
   let factory = new GMenu.factory file_menu ~accel_group in
+  factory#add_item "New file" ~key:_N ~callback:(fun () ->
+      failwith "unimplemented");
   factory#add_item "Save" ~key:_S ~callback:(fun () ->
       failwith "unimplemented");
   factory#add_item "Open file" ~key:_O ~callback:(fun () ->
-      insert_text
-        "Unimplemented, this string should come from a file read \
-         function"
-        text_field);
+      load_file editor_window);
   factory#add_item "Quit" ~key:_Q ~callback:Main.quit;
 
   (* Theme menu *)
