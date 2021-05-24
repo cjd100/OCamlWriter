24f5b011a8f7aa91b0264f4bf2c01daa247e6cbf
Nick Romanofsky <njr67@cornell.edu>
2021-05-24 02:25:14 -0400

    Moved HTML functions into a separate module

---
+++ markdown.mli
@@ -0,0 +1,5 @@
+(** Takes all of the text within a file and produces a .html file *)
+val write_html : string -> string -> unit
+
+(** Formats html text into a more readable form *)
+val format_html : string -> string
